6cb046aad384d5b22968c3af9f1d175950da7dfc
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-02-13 00:31:12 +0000

    Added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2025. Joseph Wondimagnehu. All rights reserved.
+//
+// Created by: Joseph Wondimagnehu
+// Date: Feb. 12, 2025
+// This program displays: "Hello World! With Style!"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+    cout << "Hello World! With style!" << endl;
+}
